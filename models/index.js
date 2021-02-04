0b9d8833379905191dd3c38601848a41b041a76a
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-02-04 13:21:55 +0100

    ex-3.20

--- models/index.js
+++ models/index.js
@@ -5,10 +5,12 @@ const phonebookSchema = new Schema({
     type: String,
     required: true,
     unique: true,
+    minlength: 3,
   },
   number: {
     type: String,
     required: true,
+    minlength: 8,
   },
 });
 const Person = model("Person", phonebookSchema);
